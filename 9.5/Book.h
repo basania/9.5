e71a960a95f1404322d53b509beca1a75a641419
basania <basania1996@gmail.com>
2018-01-21 21:52:11 +0200

    First commit to the second branch

--- 9.5/Book.h
+++ 9.5/Book.h
@@ -11,9 +11,9 @@ using std::endl;
 class Book
 {
 public:
-	Book(string code, string name, string last_name, string date);
+	Book(/*string code, string name, string last_name, string date*/);
 	~Book();
-	bool is_isbn_code_valid(Book object);
+	string is_isbn_code_valid(Book object);
 
 
 private:
@@ -24,13 +24,13 @@ private:
 	bool is_given;
 };
 
-Book::Book(string code, string name, string last_name, string date)
-	:isbn_code{ code }, book_name{ name }, author_last_name{ last_name }, copyright_date{ date }
+Book::Book(/*string code, string name, string last_name, string date*/)
+	//:isbn_code{ code }, book_name{ name }, author_last_name{ last_name }, copyright_date{ date }
 {
 	cout << "Constructor\n";
 }
 
 Book::~Book()
 {
-	cout << "Destructor\n";
+	//cout << "\nDestructor\n";
 }
